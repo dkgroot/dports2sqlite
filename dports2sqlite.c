89be82814e6b0e254c032cc07b91c8f0aadb1f54
ddegroot <ddegroot@talon.nl>
2019-08-13 13:07:28 +0000

    Insert ports info into sqlite db

--- dports2sqlite.c
+++ dports2sqlite.c
@@ -4,6 +4,7 @@
 #include <dirent.h>	// opendir,closedir, readdir
 #include <string.h>	// strcat, strncpy
 #include <unistd.h>	// usleep
+#include <stdarg.h>	// va_start, va_end
 //#include <sys/types.h>
 //#include <sys/stat.h>
 
@@ -21,12 +22,15 @@ threadpool pool;
 sqlite3 *db;
 
 typedef struct cat {
+    int id;
     char name[NAME_LEN];
     char dirname[DIRNAME_LEN];
 } cat_t;
 
 typedef struct port {
+    int id;
     cat_t *cat;
+    int cat_id;
     char name[NAME_LEN];
     char dirname[DIRNAME_LEN];
 } port_t;
@@ -50,10 +54,34 @@ void *process_port(void *args)
     return NULL;
 }
 
+int insert_into_db(const char *format, ...)
+{
+    int rc;
+    char *err_msg = 0;
+    va_list arg;
+    char sql[255];
+
+    va_start (arg, format);
+    vsnprintf (sql, sizeof sql - 1, format, arg);
+    va_end (arg);
+
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    if (rc != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        return -1;
+    }
+    return sqlite3_last_insert_rowid(db);
+}
+
 void process_cat(cat_t *cat)
 {
     struct dirent *portdir = NULL;
     printf ("- category: %s\n", cat->name);
+    int cat_id = insert_into_db("INSERT INTO Category(Name, DirName) VALUES('%s', '%s');", cat->dirname, cat->dirname);
+    if (cat_id < 0) return;
+    cat->id = cat_id;
+    
     char cat_dir_str[PATH_LEN]="";
     strcat(cat_dir_str, dports_dir);
     strcat(cat_dir_str, cat->dirname);
@@ -63,10 +91,14 @@ void process_cat(cat_t *cat)
         while((portdir = readdir(cat_dir)))
         {
             if (portdir->d_type == DT_DIR && portdir->d_name[0] != '.') {
-                printf ("   - port: %s\n", portdir->d_name);
+                int port_id = insert_into_db("INSERT INTO Port(PkgName, DirName, MaintainerId, CatId) VALUES('%s', '%s', 0, %d);", portdir->d_name, portdir->d_name, cat_id);
+                if (port_id < 0) return; 
+                //printf ("   - port: %s\n", portdir->d_name);
                 port_t *port = calloc(1, sizeof(port_t));			// freed by process_port
                 if (port) {
+                    port->id = port_id;
                     port->cat = cat;
+                    port->cat_id = cat_id;
                     strncpy(port->name, portdir->d_name, sizeof port->name - 1);
                     strncpy(port->dirname, portdir->d_name, sizeof port->name - 1);
                     thpool_add_work(pool, (void *)process_port, (void *)port);
@@ -94,15 +126,48 @@ void process_dports()
     }
 }
 
+int setup_sqlite()
+{
+    int rc;
+    char *err_msg = 0;
+    char *sql = "DROP TABLE IF EXISTS Category;"
+                "DROP TABLE IF EXISTS Port;"
+                "DROP TABLE IF EXISTS Deps;"
+                "DROP TABLE IF EXISTS Maintainer;"
+                "DROP TABLE IF EXISTS DepTypes;"
+                "CREATE TABLE Category(Id INT PRIMARY KEY, Name TEXT, DirName TEXT);"
+                "CREATE TABLE Maintainer(Id INT PRIMARY KEY, Name TEXT, Email TEXT);"
+                "CREATE TABLE Port(Id INT PRIMARY KEY, PkgName TEXT, DirName TEXT, Path TEXT, Prefix TEXT Comment TEXT, Description TEXT,"
+                    "MaintainerId INT NOT NULL REFERENCES Maintainer(Id), CatId INT NOT NULL REFERENCES Category(Id));"
+                "CREATE TABLE DepTypes(Type CHAR(1) PRIMARY KEY, Name TEXT);"
+                "INSERT INTO DepTypes VALUES('B', 'Build');"
+                "INSERT INTO DepTypes VALUES('R', 'Run');"
+                "INSERT INTO DepTypes VALUES('E', 'Extract');"
+                "INSERT INTO DepTypes VALUES('P', 'Patch');"
+                "INSERT INTO DepTypes VALUES('F', 'Fetch');"
+                "CREATE TABLE Deps(PortId INT NOT NULL REFERENCES Port(Id), DepPortId INT NOT NULL REFERENCES Port(Id), DepType CHAR(1) NOT NULL DEFAULT ('B') REFERENCES DepTypes(Type))";
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    if (rc != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        return -1;
+    }
+    return 0;
+}
+
+
 int main(int argc, char** argv)
 {
     printf("Generating dport sqlite3 db\n");
 
     int rc;
     rc = sqlite3_open("index.db", &db);
-    if(rc){
+    if (rc != SQLITE_OK){
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
         exit(1);
+    }
+    if (setup_sqlite()) {
+        goto close_db;
     }
     
     pool = thpool_init(num_threads);
